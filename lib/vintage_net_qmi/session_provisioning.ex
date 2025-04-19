d70b87a69f12fff8d3c0a351354555ee4e556e0e
Marc Lainez <marc.lainez@gmail.com>
2025-04-19 10:18:05 +0200

    Set modem online sooner

--- lib/vintage_net_qmi/session_provisioning.ex
+++ lib/vintage_net_qmi/session_provisioning.ex
@@ -25,6 +25,7 @@ defmodule VintageNetQMI.SessionProvisioning do
       active: false
     }
 
+    :ok = DeviceManagement.set_operating_mode(state.qmi, :online)
     card_status = UserIdentity.get_cards_status(state.qmi)
     Logger.warning("[VintageNetQMI] Card status: #{inspect(card_status, limit: :infinity)}")
     case extract_slot_id_and_application_id(card_status) do
@@ -36,7 +37,6 @@ defmodule VintageNetQMI.SessionProvisioning do
         {:ok, state}
       {slot_id, application_id} ->
         {:ok} = UserIdentity.provision_uim_session(state.qmi, slot_id, application_id)
-        :ok = DeviceManagement.set_operating_mode(state.qmi, :online)
         {:ok, %{state | active: true, slot_id: slot_id, application_id: application_id}}
     end
   end
